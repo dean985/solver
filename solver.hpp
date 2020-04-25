773c822dc93b957a829159a596dab57c7ee8fe66
Dean <carmideanGmail.com>
2020-04-25 23:43:23 +0300

    instruction and classes added

---
+++ solver.hpp
@@ -0,0 +1,27 @@
+/**
+ * solving single variable equation 
+ * Either linear or quadratic equations.
+ * 
+ * Domain and range are complex and real numbers.
+ * 
+ * if there is more then one solution- return one of them randomly 
+ * for tests - (7 == 7.0) is True. 
+ * 
+ * Throw Exceptions:
+ * More then one variable
+ * Inequality 
+ * Exponent higher than two
+ * No solution for given equation
+ */
+
+
+namespace solver{
+    class RealVariable{
+    };
+
+    class ComplexVariable{
+    };
+
+    class solve{
+    };
+}
