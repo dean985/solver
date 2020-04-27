58e7b523698226f99adb173c9a7a200462222569
Ubuntu <ubuntu@ip-172-31-17-19.us-east-2.compute.internal>
2020-04-27 07:38:46 +0000

    added test.cpp

---
+++ Test.cpp
@@ -0,0 +1,19 @@
+#include "doctest.h"
+#include "solver.hpp"
+#include <string>
+using namespace std;
+using namespace solver;
+
+/**
+ *                                        yosef
+ *                  avi                                            rivka
+ *        mira             moshe                        israel              rina
+ * 
+ * 
+ */
+
+
+TEST_CASE("Tests for realvarible class")
+{
+    
+}
