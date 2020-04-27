231a5c6c570e4683d663318a7bbe456a66f4b08b
Dean <carmidean@gmail.com>
2020-04-27 11:48:51 +0300

    adding cpp file

---
+++ solver.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "solver.hpp"
+
+using namespace std;
+namespace solver{
+
+/**
+ * Complex variable memeber function implementation
+ */
+
+
+
+
+}
--- solver.hpp
+++ solver.hpp
@@ -13,15 +13,62 @@
  * Exponent higher than two
  * No solution for given equation
  */
-
-
+#include <complex>
+using namespace std;
 namespace solver{
-    class RealVariable{
+    class RealVariable {
     };
 
     class ComplexVariable{
+        complex<double> c1;
+        complex<double> c2;
+        complex<double> c3;
+
+    public:
+    ComplexVariable() : c1(0), c2(1.0), c3(0) {}
+
+    ComplexVariable(const complex<double> a1, const complex<double> a2, const complex<double> a3){
+      c1 = complex(a1); c2 = complex(a2); c3 = complex(a3);
+    }
+    //Getters
+    //TODO: Is there a need for getters ?
+//    const complex<double>& get_c1() const{
+//      return c1;
+//    }
+//    const complex<double>& get_c2() const{
+//      return c2;
+//    }
+//    const complex<double>& get_c3() const{
+//      return c3;
+//    }
+
+    //Operator overloading
+    friend ComplexVariable operator+(const ComplexVariable& var, const complex<double>& c);
+    friend ComplexVariable operator+(const complex<double>& c, const ComplexVariable& var);
+    friend ComplexVariable operator+(const ComplexVariable& a, const ComplexVariable& b);
+
+    friend ComplexVariable operator-(const ComplexVariable& var, const complex<double>& c);
+    friend ComplexVariable operator-(const complex<double>& c ,const ComplexVariable& var);
+    friend ComplexVariable operator-(const ComplexVariable& a,const ComplexVariable& b);
+
+    friend ComplexVariable operator*(const ComplexVariable& var, const complex<double>& c);
+    friend ComplexVariable operator*(const complex<double>& c ,const ComplexVariable& var);
+    friend ComplexVariable operator*(const ComplexVariable& a,const ComplexVariable& b);
+
+    friend ComplexVariable operator/(const ComplexVariable& var, const complex<double> c);
+    friend ComplexVariable operator/(const ComplexVariable& a, const ComplexVariable& b);
+
+    friend ComplexVariable operator^(const ComplexVariable& var, const complex<double> c);
+
+    //ComplexVariable operator- () const;
+
+    friend ComplexVariable operator==(const ComplexVariable& a,const ComplexVariable& b);
+    friend ComplexVariable operator==(const complex<double> c,const ComplexVariable& c_var);
+    friend ComplexVariable operator==(const ComplexVariable& c_var, const complex<double> c);
+
+    //Operator for nagative ComplexVariable
+    ComplexVariable operator -();
+    
     };
 
-    class solve{
-    };
 }
